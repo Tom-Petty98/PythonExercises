8e78bc1c2b60ead4941de3f554ee760190b46c5c
Tom Petty <tompetty527@yahoo.co.uk>
2020-05-06 10:06:02 +0100

    Reorganizing files in a more logical fashion

--- AdditionalExercises/RoutePlanner.py
+++ AdditionalExercises/RoutePlanner.py
@@ -1,5 +1,7 @@
-# just trying to generate the longest list possible whilst foolowing the rules
-# try each number and see how long the list it creates is
+# just trying to generate the longest list possible whilst following the rules
+# try each possibility and see how long the list it creates is, maybe have two list storing the longest list 
+# found and the current list and compare the two and remove the shorter one, 
+# then use the empty list to try the next possibility
 # select smallest number in first half and start from their
 # potentially i could sort the numbers then find their original indexes later
 import sys
@@ -32,4 +34,4 @@ def plan_route2(listOfPeaks):
 
 
 listOfPeaks = [1, 2, 20, 13, 6, 15, 16, 0, 7, 9, 4, 0, 4, 6, 7, 8, 10, 18, 14, 10, 17, 15, 19, 0, 4, 2, 12, 6, 10, 5, 12]
-print(numpy.argsort(listOfPeaks))
+print(numpy.argsort(listOfPeaks))   # out [23 7 11 0 1 25 24 10 12 29 27 4 13 8 14 15 9 16 19 28 26 30 3 18 21 5 6 20 17 22 2]
